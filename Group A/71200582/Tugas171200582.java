f6f4daeaa29ff02a27c0183fbf9f020a47ca0542
MaverickMM <78334716+MaverickMM@users.noreply.github.com>
2022-02-13 12:07:08 +0700

    Tugas1-71200582

---
+++ Group A/71200582/Tugas171200582.java
@@ -0,0 +1,13 @@
+public class Tugas171200582{
+    public static void main(String[] args) {
+      
+        int n = 582;
+        int jumlah = 0;
+        
+        for(int i=1; i<=n; i++) {
+            jumlah = jumlah + i;
+          }
+        System.out.println("3 DIGIT NIM: " + n + " TOTAL = " + jumlah + " 71200582 Maverick Mikolas Mulyadi");
+
+    }
+}
